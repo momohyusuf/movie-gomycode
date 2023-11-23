38131aa0d06a8f02b4a441155c62e3c4e763c35e
Momoh yusuf <ymomoh6@outlook.com>
2023-11-23 09:03:47 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import { Button } from "antd";
+function App() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+      <Button type="primary">Button</Button>
+    </div>
+  );
+}
+
+export default App;
